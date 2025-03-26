1dab6e49dd1c5188857f3cf072628f37d798de50
Monika <mciskowska@gmail.com>
2025-03-26 15:02:15 +0000

    create basic new topic page- link to it by button from home page

---
+++ src/app/new_topic/page.tsx
@@ -0,0 +1,14 @@
+import Header from "../components/header/header";
+import Footer from "../components/footer/footer";
+export default function New_topic(){
+    return (
+        <div>
+        <Header></Header>
+        
+        <p>This is new topic page</p>
+        <h1>What do you want to remember?</h1>
+       
+        <Footer></Footer>
+        </div>
+    )
+}
