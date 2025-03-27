6f0218e5878d1fcae6ebf3bce8eb2e54a0fab4c0
Monika <mciskowska@gmail.com>
2025-03-27 14:11:13 +0000

    create return "back" button with correct route to go back to the previous page

--- src/app/new_topic/page.tsx
+++ src/app/new_topic/page.tsx
@@ -2,6 +2,7 @@ import Header from "../components/header/header";
 import Footer from "../components/footer/footer";
 import NavButton from "../components/navButton/navButton";
 import TextForm from "../components/textForm/textForm";
+import Link from "next/link";
 
 export default function New_topic() {
   return (
@@ -11,8 +12,10 @@ export default function New_topic() {
       <p>This is new topic page</p>
       <h1>What do you want to remember?</h1>
       <TextForm/>
-
-      <NavButton text="Next" />
+      <Link href="/">
+      <NavButton text="Back" />
+      </Link>
+      
 
       <Footer></Footer>
     </div>
