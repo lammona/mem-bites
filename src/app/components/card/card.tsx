0bd309402f463292922dd235708b5d9582fcc2ff
Monika <mciskowska@gmail.com>
2025-05-14 12:53:37 +0100

    wrapped "approve and "remove" buttons in action div together with the card component displayed

--- src/app/components/card/card.tsx
+++ src/app/components/card/card.tsx
@@ -4,13 +4,28 @@ interface CardProps {
   content: string;
 }
 
-export default function Card( {content}: CardProps) {
+// export default function Card( {content}: CardProps) {
+//   return (
+//     <div>
+//       <p className={styles.card}>
+//         {content}
+//       </p>
+//       <button className={styles.approve}>✅</button>
+//       <button className={styles.remove}>❌</button>
+//     </div>
+//   );
+// }
+
+export default function Card({content}: CardProps){
   return (
-    <div>
-      <p className={styles.card}>
-        {content}
-      </p>
-      <button>✅</button>
+    <div className={styles.card}>
+      <div className={styles.cardContent}>
+        <p>{content}</p>
+        <div className={styles.actions}>
+        <button className={styles.approve}>Approve ✅</button>
+        <button className={styles.remove}>Remove❌</button>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
